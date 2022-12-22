8535ac60b4b51a7625f4588d0222f1438fab627f
Saurabhrajput1221 <95747934+Saurabhrajput1221@users.noreply.github.com>
2022-12-22 14:01:56 +0530

    final commit

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,5 +5,13 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+        if(isVeg){
+            this.cheesePrice = 80;
+            this.toppingPrice = 70;
+
+        }else{
+            this.cheesePrice = 80;
+            this.toppingPrice = 120;
+        }
     }
 }
